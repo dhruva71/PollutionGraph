2c1e4aefd4e66757a6d25ac788cc79c3e3d86160
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2023-03-25 19:04:51 +0530

    Normalized date

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ pg = PollutionGraph(config=st.secrets)
 
 # Set start and end time
 # get the first day of the current month
-START = int(pd.to_datetime("today").replace(day=1).timestamp())
-END = int(pd.to_datetime("today").timestamp())
+START = int(pd.to_datetime("today").replace(day=1).normalize().timestamp())
+END = int(pd.to_datetime("today").normalize().timestamp())
 
 # if you wish to let the users pick the start and end date, uncomment the following lines
 # START = st.date_input("Start date", value=pd.to_datetime("2023-03-01"))
